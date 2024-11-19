6eb930da3fdc2f68bbe65197ce6d248ab192c3c5
mariFeldk <118355692+mariFeldk@users.noreply.github.com>
2024-11-19 07:50:34 -0400

    Maria
    instalei os pacotes (cors, express e mysql2)

---
+++ src/main.ts
@@ -0,0 +1,64 @@
+
+// 1 - Para construir um servidor back-end e responder
+// Vamos utilizar o EXPRESS
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql2/promise'
+//Criar um objeto do tipo express.
+const app = express()
+//incluir pra ele receber json
+app.use(express.json())  //Middleware
+//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
+app.use(cors())
+//ROTAS
+app.get("/produtos",async(req,res)=>{
+
+    //O que eu tenho que fazer aqui dentro?
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        //PASSO 3: QUERY  -> SELECT * FROM produtos
+        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
+        await conexao.end()
+        //PASSO 4: Colocar os dados do banco no response
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+app.post("/produtos",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                [id,nome,descricao,preco,imagem])
+        await conexao.end()
+        res.status(200).send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+
+
+
+//INICIAR O SERVIDOR
+app.listen(8000,()=>{
+    console.log("SERVIDOR INICIADO NA PORTA 8000")
+})
