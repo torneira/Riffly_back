e5bb9d5bb9726d6010521078f8a43786fff4d07c
mariFeldk <118355692+mariFeldk@users.noreply.github.com>
2024-11-25 07:54:33 -0400

    rotas
    fiz as rotas e arrumei o commit

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,12 @@ app.use(cors())
 //ROTAS
 app.get("/produtos",async(req,res)=>{
 
-    //O que eu tenho que fazer aqui dentro?
-    //OK -> PASSO 1: Criar o banco de dados
-    //PASSO 2: Usar a lib mysql2 para conectar com o banco
     try{
         const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022b",
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         //PASSO 3: QUERY  -> SELECT * FROM produtos
@@ -37,10 +34,10 @@ app.get("/produtos",async(req,res)=>{
 app.post("/produtos",async(req,res)=>{
     try{
         const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022b",
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         const {id,nome,descricao,preco,imagem} = req.body
@@ -56,6 +53,86 @@ app.post("/produtos",async(req,res)=>{
 })
 
 
+app.get("/usuarios",async(req,res)=>{
+
+
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const [result,fields]  = await conexao.query("SELECT * FROM usuarios")
+        await conexao.end()
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+app.post("/usuarios",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id, nome_usuario, email_usuario, senha_usuario, foto_usuario} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                [id, nome_usuario, email_usuario, senha_usuario, foto_usuario])
+        await conexao.end()
+        res.status(200).send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+app.get("/album",async(req,res)=>{
+
+
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        
+        const [result,fields]  = await conexao.query("SELECT * FROM album")
+        await conexao.end()
+        res.send(result)
+    }catch(e){
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+app.post("/album",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost,
+            user:process.env.dbuser,
+            password:process.env.dbpassword,
+            database:process.env.dbname,
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id, nome_album, cantor_musica, quant_musicas_album, lancamento_album, capa_album, ouvintes_album} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?,?,?)",
+                [id, nome_album, cantor_musica, quant_musicas_album, lancamento_album, capa_album, ouvintes_album])
+        await conexao.end() 
+        res.status(200).send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
 
 
 //INICIAR O SERVIDOR
